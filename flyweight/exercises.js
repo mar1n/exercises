592ecaab565e6a8d6863cf732c3c8ff55aa397d8
mar1n <30738216+mar1n@users.noreply.github.com>
2024-03-12 23:32:47 +0000

    flyweight exercises

--- flyweight/exercises.js
+++ flyweight/exercises.js
@@ -8,20 +8,86 @@ const implementsInterface = (obj, iface) => {
 };
 
 
-const setFont = ["font", "size", "display"];
+const setFont = ["getFont", "getSize", "display"];
 
 class CharacterStyle {
-    constrcutor(font, size) {
-        this.font = font;
+    constructor(fontName, size) {
+        this.fontName = fontName;
         this.size = size;
     }
-    font() {
-        return this.font;
+    getFont() {
+        return this.fontName;
     }
-    size() {
+    getSize() {
         return this.size;
     }
-    display(self, character) {
-        console.log(`${character} character has Font ${this.font} and size ${this.size}`);
+    display(character) {
+        console.log(`${character.getCharacter()} character has Font ${this.fontName} and size ${this.size}`);
     }
 }
+
+const characterContext = (character) => ({
+    getCharacter() {
+        return character;
+    }
+})
+
+class StyleFactory {
+    constructor() {
+        this.fontStyles = {};
+        this.length = 0;
+    }
+
+    getFontStyle(fontName, size) {
+        let font = this.fontStyles[fontName];
+        if(!font) {
+            console.log("fontName", fontName, "size", size);
+            font = new CharacterStyle(fontName, size);
+            console.log("font", font);
+            if(implementsInterface(font, setFont)) {
+                this.fontStyles[fontName] = font;
+                this.length++;
+            } else {
+                console.log(
+                    `Error: ${fontName} does not implement the CoffeOrder interface.`
+                );
+                return null;
+            }
+        }
+        return this.fontStyles[fontName] = new CharacterStyle(fontName, size);
+    }
+
+    getTotalFontsCount() {
+        return this.length;
+    }
+}
+
+function txtEditor() {
+    const fonts = [];
+    const characters = [];
+    let fontSizes = 0;
+    const fontFactory = new StyleFactory();
+
+    function addCharacter(char, fontName, size) {
+        const font = fontFactory.getFontStyle(fontName, size);
+        if(font) {
+            fonts.push(font);
+            characters.push(characterContext(char));
+            fontSizes++
+        }
+    }
+
+    addCharacter("r", "Times New Roman", 10);
+    addCharacter("b", "News Gothic", 7);
+    addCharacter("a", "Courier", 12);
+    addCharacter("f", "Lucida", 5);
+    addCharacter("g", "Lucida", 6);
+
+
+    for(let i =0; i< fontSizes; ++i) {
+        fonts[i].display(characters[i]);
+    }
+    console.log(`Total fonts: ${fontFactory.getTotalFontsCount()}`);
+}
+
+txtEditor();
